e40525e10a50c6d60e25e3ad59c79af55e79873a
Sachin Jayadev <sachinjayadev18@gmail.com>
2023-12-16 01:16:18 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://chatrooms-client.vercel.app/",
+        origin: "https://chatrooms-client.vercel.app",
         methods: ["GET", "POST"], 
     },
 });
